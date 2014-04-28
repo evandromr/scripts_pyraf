1dd18a284f3e926b7072eb2d0747772258252165
Evandro Martinez Ribeiro <evandromartinezribeiro@gmail.com>
2014-04-28 09:35:58 -0300

    calibrate wavelenght with wavescale.py

---
+++ wavescale.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+#
+# Find wavelenght solution to a calibration spectra
+#
+import glob
+from pyraf import iraf
+
+iraf.onedspec()
+iraf.onedspec.unlearn()
+iraf.onedspec.identify.unlearn()
+
+re = str(raw_input("Enter regular expression for input spectra: "))
+calspec = glob.glob(re)[0]
+
+linelist = str(raw_input(
+'Enter name of iraf file with list of lines (linelists$thar.dat): '))
+
+if linelist == '':
+    linelist = 'linelists$thar.dat'
+
+iraf.onedspec.identify.coordlist = linelist
+iraf.onedspec.identify(images=calspec)
