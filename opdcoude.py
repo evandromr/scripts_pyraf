0ffd4768c8b36826d680d3d6c8d70638329ef93f
Evandro Martinez Ribeiro <evandromartinezribeiro@gmail.com>
2014-04-30 16:36:10 -0300

    change name to default

--- opdcoude.py
+++ opdcoude.py
@@ -5,73 +5,89 @@
 #
 # Load Python standard modules
 import glob
+import os
+import shutil
 # Load third-party modules
 import ds9
-import astropy.io.fits as fits
 from pyraf import iraf
 
 
 print 'Loading IRAF packages ...'
 iraf.imred()
 iraf.ccdred()
-iraf.onedspec()
-iraf.twodspec()
-iraf.apextract()
+iraf.specred()
 
 print 'unlearning previous settings...'
+iraf.ccdred.unlearn()
 iraf.ccdred.ccdproc.unlearn()
 iraf.ccdred.combine.unlearn()
 iraf.ccdred.zerocombine.unlearn()
 iraf.ccdred.flatcombine.unlearn()
-iraf.apextract.unlearn()
-iraf.apall.unlearn()
-iraf.onedspec.identify.unlearn()
-iraf.onedspec.dispcor.unlearn()
+iraf.specred.response.unlearn()
+iraf.specred.apall.unlearn()
+iraf.specred.identify.unlearn()
+iraf.specred.dispcor.unlearn()
+iraf.imstatistics.unlearn()
+iraf.imexamine.unlearn()
+iraf.hedit.unlearn()
+iraf.splot.unlearn()
 iraf.continuum.unlearn()
 
-# define instrument parameter for ccdred (see iraf tasks documentation)
-iraf.ccdred.instrument = 'coude'
+# regular expression of files (e.g bias_00*.fits, flat-2000jan01_?.*)
+zerore = str(raw_input('\nEnter regular expression for zero level images: '))
+dflatre = str(raw_input('Enter regular expression for dome flat images: '))
+iflatre = str(raw_input('Enter regular expression for internal flat images: '))
+scire = str(raw_input('Enter regular expression for science images: '))
+calre = str(raw_input('Enter regular expression for calibration images: '))
+
+# list of files that match that regular expression
+zerolist = glob.glob(zerore)
+zeroin = ', '.join(zerolist)
+
+dflatlist = glob.glob(dflatre)
+dflatin = ', '.join(dflatlist)
+dflatzeros = ', '.join([df[:-5]+'_zero.fits' for df in dflatlist])
+
+iflatlist = glob.glob(iflatre)
+iflatin = ', '.join(iflatlist)
+iflatzeros = ', '.join([ifl[:-5]+'_zero.fits' for ifl in iflatlist])
+
+scilist = glob.glob(scire)
+sciin = ', '.join(scilist)
+scizeros = ', '.join([sc[:-5]+'_zero.fits' for sc in scilist])
+sciproc = ', '.join([sc[:-5]+'_proc.fits' for sc in scilist])
+sciap = ', '.join([sc[:-5]+'_proc.ms.fits' for sc in scilist])
+scispec = ', '.join([sc[:-5]+'_spec.fits' for sc in scilist])
+
+callist = glob.glob(calre)
+calin = ', '.join(callist)
+calzeros = ', '.join([cal[:-5]+'_zero.fits' for cal in callist])
+calproc = ', '.join([cal[:-5]+'_proc.fits' for cal in callist])
+calap = ', '.join([cal[:-5]+'_proc.ms.fits' for cal in callist])
 
 print 'openning a ds9 window if not already openned...'
 ds9.ds9()
 
-# regular expression of files (e.g bias_00*.fits, flat-2000jan01_?.*)
-biasre = str(raw_input('\nEnter regular expression for Bias images: '))
-flatsre = str(raw_input('Enter regular expression for Flat images: '))
-sciencere = str(raw_input('Enter regular expression for Science images: '))
-calre = str(raw_input('Enter regular expression for Calibration images: '))
+print 'creating a raw_files/ dir to store original data'
+if not os.path.isdir('raw_files'):
+    os.mkdir('raw_files')
 
-# list of files that match that regular expression
-biases = glob.glob(biasre)
-flatses = glob.glob(flatsre)
-sciences = glob.glob(sciencere)
-calses = glob.glob(calre)
-
-## Transforms python list in strings to be used by pyraf tasks
-biasstring = ', '.join(biases)
-flatsstring = ', '.join(flatses)
-sciencestring = ', '.join(sciences)
-calstring = ', '.join(calses)
-# other science lists
-scienceprocstring = ', '.join([sc[:-5] + '_proc.fits' for sc in sciences])
-scienceapalstring = ', '.join([sc[:-5] + '_proc.0001.fits' for sc in sciences])
-sciencespecstring = ', '.join([sc[:-5] + '_spec.fits' for sc in sciences])
-# other calibration lists
-calprocstring = ', '.join([cal[:-5] + '_proc.fits' for cal in calses])
-calapalstring = ', '.join([cal[:-5] + '_proc.0001.fits' for cal in calses])
+allfiles = zerolist + dflatlist + iflatlist + scilist + callist
+for file in allfiles:
+    shutil.copy(file, 'raw_files/')
 
 # combine bias images
 print 'Combining Bias images...'
-iraf.ccdred.ccdproc.unlearn()
-iraf.ccdred.combine.unlearn()
-iraf.ccdred.zerocombine.unlearn()
 iraf.ccdred.zerocombine.output = 'Zero'
-iraf.ccdred.zerocombine.combine = 'average'
-iraf.ccdred.zerocombine.reject = 'minmax'
+iraf.ccdred.zerocombine.combine = 'median'
+iraf.ccdred.zerocombine.reject = 'ccdclip'
 iraf.ccdred.zerocombine.ccdtype = ''
-iraf.ccdred.zerocombine.rdnoise = fits.getval(biases[0], 'RDNOISE')
-iraf.ccdred.zerocombine.gain = fits.getval(biases[0], 'GAIN')
-iraf.ccdred.zerocombine(input=biasstring)
+iraf.ccdred.zerocombine.rdnoise = 'RDNOISE'
+iraf.ccdred.zerocombine.gain = 'GAIN'
+iraf.ccdred.zerocombine(input=zeroin)
+
+for zero in zerolist:
+    os.remove(zero)
 
 # check output bias image
 print 'Check output file:'
@@ -79,41 +95,7 @@ iraf.imstatistics('Zero')
 print ' Running "imexamine" task..'
 iraf.imexamine('Zero', 1)
 
-# combine flat images
-print 'Combining Flat images ...'
-iraf.ccdred.ccdproc.unlearn()
-iraf.ccdred.combine.unlearn()
-iraf.ccdred.flatcombine.unlearn()
-iraf.ccdred.flatcombine.output = 'Flat'
-iraf.ccdred.flatcombine.combine = 'median'
-iraf.ccdred.flatcombine.reject = 'sigclip'
-iraf.ccdred.flatcombine.process = 'no'
-iraf.ccdred.flatcombine.subsets = 'no'
-iraf.ccdred.flatcombine.scale = 'mode'
-iraf.ccdred.flatcombine.ccdtype = ''
-iraf.ccdred.flatcombine.rdnoise = fits.getval(flatses[0], 'RDNOISE')
-iraf.ccdred.flatcombine.gain = fits.getval(flatses[0], 'GAIN')
-iraf.ccdred.flatcombine(input=flatsstring)
-
-# check output flat image
-print 'Check output file:'
-iraf.imstatistics('Flat')
-print ' Running "imexamine" task..'
-iraf.imexamine('Flat', 1)
-
-trimsection = str(raw_input(
-'Enter trim section [col:umns,li:nes], Or hit <Enter>: '))
-
-if trimsection == '':
-    trimsection = '[{0:d}:{1:d},{0:d}:{2:d}]'.format(
-    1, fits.getval(sciences[0], 'NAXIS1'), fits.getval(sciences[0], 'NAXIS2'))
-
-# correct images
-print ''
-print 'Running ccdproc task ...'
-print ''
-iraf.ccdred.ccdproc.unlearn()
-iraf.ccdred.combine.unlearn()
+print 'Running zero level correction ...'
 iraf.ccdred.ccdproc.ccdtype = ''
 iraf.ccdred.ccdproc.noproc = False
 iraf.ccdred.ccdproc.fixpix = False
@@ -123,38 +105,119 @@ iraf.ccdred.ccdproc.illumcor = False
 iraf.ccdred.ccdproc.fringecor = False
 iraf.ccdred.ccdproc.readcor = False
 iraf.ccdred.ccdproc.scancor = False
-iraf.ccdred.ccdproc.trim = True
-iraf.ccdred.ccdproc.trimsec = trimsection
 iraf.ccdred.ccdproc.readaxis = 'line'
+iraf.ccdred.ccdproc.trim = False
+iraf.ccdred.ccdproc.flatcor = False
 iraf.ccdred.ccdproc.zerocor = True
 iraf.ccdred.ccdproc.zero = 'Zero'
-iraf.ccdred.ccdproc.flatcor = True
-iraf.ccdred.ccdproc.flat = 'Flat'
 # science images
-iraf.ccdred.ccdproc.output = scienceprocstring
-iraf.ccdred.ccdproc(images=sciencestring)
-# calibration images (same parameters)
-iraf.ccdred.ccdproc.output = calprocstring
-iraf.ccdred.ccdproc(images=calstring)
+iraf.ccdred.ccdproc(images=sciin)
+# calibration images
+iraf.ccdred.ccdproc(images=calin)
+# dome flats
+iraf.ccdred.ccdproc(images=dflatin)
+# internal flats
+iraf.ccdred.ccdproc(images=iflatin)
+
+# combine flat images
+print 'Combining dome flat images ...'
+iraf.ccdred.flatcombine.output = 'dFlat'
+iraf.ccdred.flatcombine.combine = 'median'
+iraf.ccdred.flatcombine.reject = 'ccdclip'
+iraf.ccdred.flatcombine.process = 'no'
+iraf.ccdred.flatcombine.subsets = 'no'
+iraf.ccdred.flatcombine.scale = 'mode'
+iraf.ccdred.flatcombine.ccdtype = ''
+iraf.ccdred.flatcombine.rdnoise = 'RDNOISE'
+iraf.ccdred.flatcombine.gain = 'GAIN'
+iraf.ccdred.flatcombine(input=dflatin)
+for file in dflatlist:
+    os.remove(file)
+
+# check output flat image
+print 'Check output file:'
+iraf.imstatistics('dFlat')
+print ' Running "imexamine" task..'
+iraf.imexamine('dFlat', 1)
+
+print 'Combining internal flat images ...'
+iraf.ccdred.flatcombine.output = 'iFlat'
+iraf.ccdred.flatcombine(input=iflatin)
+for file in iflatlist:
+    os.remove(file)
+
+# check output flat image
+print 'Check output file:'
+iraf.imstatistics('iFlat')
+print ' Running "imexamine" task..'
+iraf.imexamine('iFlat', 1)
+
+iraf.specred.extinction = ''
+iraf.specred.caldir = ''
+iraf.specred.observatory = 'lna'
+
+print 'Create a response for dome flat...'
+iraf.specred.response.interactive = True
+iraf.specred.response(calibration='dFlat',
+                      normalization='dFlat',
+                      response='ndFlat')
+# check output flat image
+print 'Check output file:'
+iraf.imstatistics('ndFlat')
+print ' Running "imexamine" task..'
+iraf.imexamine('ndFlat', 1)
+
+print 'Create a response for internal flat...'
+iraf.specred.response.interactive = True
+iraf.specred.response(calibration='iFlat',
+                      normalization='iFlat',
+                      response='niFlat')
+# check output flat image
+print 'Check output file:'
+iraf.imstatistics('niFlat')
+print ' Running "imexamine" task..'
+iraf.imexamine('niFlat', 1)
+
+
+# Trim data if necessary
+trimquery = True
+trimsection = str(raw_input('Enter trim section, or hit <Enter>: '))
+if trimsection == '':
+    trimquery = False
+
+# correct images
+print 'Running Flat and Trimming correction...'
+iraf.ccdred.ccdproc.ccdtype = ''
+iraf.ccdred.ccdproc.noproc = False
+iraf.ccdred.ccdproc.fixpix = False
+iraf.ccdred.ccdproc.overscan = False
+iraf.ccdred.ccdproc.darkcor = False
+iraf.ccdred.ccdproc.illumcor = False
+iraf.ccdred.ccdproc.fringecor = False
+iraf.ccdred.ccdproc.readcor = False
+iraf.ccdred.ccdproc.scancor = False
+iraf.ccdred.ccdproc.readaxis = 'line'
+iraf.ccdred.ccdproc.zerocor = False
+iraf.ccdred.ccdproc.flatcor = True
+iraf.ccdred.ccdproc.trim = trimquery
+iraf.ccdred.ccdproc.trimsec = trimsection
+# science images
+iraf.ccdred.ccdproc.flat = 'ndFlat'
+iraf.ccdred.ccdproc(images=sciin)
+# calibration images
+iraf.ccdred.ccdproc.flat = 'niFlat'
+iraf.ccdred.ccdproc(images=calin)
 
 # extract aperture spectra for science images
 print 'Extracting aperture spectra for science images ...'
-iraf.apextract.unlearn()
-iraf.apall.unlearn()
-iraf.apextract.dispaxis = 2
-iraf.apall.format = 'onedspec'
-iraf.apall.readnoise = fits.getval(sciences[0], 'RDNOISE')
-iraf.apall.gain = fits.getval(sciences[0], 'GAIN')
-iraf.apall(input=scienceprocstring)
+iraf.specred.apall.readnoise = 'RDNOISE'
+iraf.specred.apall.gain = 'GAIN'
+iraf.specred.apall(input=sciin)
 
 # extract aperture spectra for calibration images
 print 'Extracting aperture spectra for calibration images ...'
-iraf.apextract.unlearn()
-iraf.apall.unlearn()
-iraf.apextract.dispaxis = 2
-iraf.apall.format = 'onedspec'
-iraf.apall.readnoise = fits.getval(calses[0], 'RDNOISE')
-iraf.apall.gain = fits.getval(calses[0], 'GAIN')
-iraf.apall(input=calprocstring)
+iraf.specred.apall.readnoise = 'RDNOISE'
+iraf.specred.apall.gain = 'GAIN'
+iraf.specred.apall(input=calin)
 
 print '--- DONE ---'
