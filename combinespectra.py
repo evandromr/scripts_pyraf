ad983769150909b1e6029ab119259a721ff3cf98
Evandro Martinez Ribeiro <evandromartinezribeiro@gmail.com>
2014-05-01 07:55:18 -0300

    dont need astropy, use RDNOISE and GAIN keyword

--- combinespectra.py
+++ combinespectra.py
@@ -5,7 +5,6 @@
 # Load Python standard modules
 import glob
 # Load third-party modules
-import astropy.io.fits as fits
 from pyraf import iraf
 
 
@@ -20,22 +19,12 @@ specout = str(raw_input("Enter output combined spectra name: "))
 # unlearn previous settings
 iraf.scombine.unlearn()
 
-# Unse gain and readout noise from header or user defined
-print "Using READNOISE and GAIN keywords of first spectra"
-check = str(raw_input("Manually choose gain and rdnoise? Y/N: "))
-if (check == 'Y') or (check == 'y'):
-    rdnoise = float(raw_input("Enter ReadOutNoise: "))
-    gain = float(raw_input("Enter Gain: "))
-else:
-    rdnoise = fits.getval(specfiles[0], 'RDNOISE')
-    gain = fits.getval(specfiles[0], 'GAIN')
-
 # setup
 iraf.scombine.group = 'all'
 iraf.scombine.combine = 'median'
-iraf.scombine.reject = 'sigclip'
-iraf.scombine.rdnoise = rdnoise
-iraf.scombine.gain = gain
+iraf.scombine.reject = 'ccdclip'
+iraf.scombine.rdnoise = 'RDNOISE'
+iraf.scombine.gain = 'COMBINE'
 
 # call scombine
 iraf.scombine(input=specstring, output=specout)
